1942ca756a2fa553d1eced28560de24e9720a4c5
Mobolaji Abdulsalam <56216197+moriire@users.noreply.github.com>
2020-05-16 03:10:16 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Calx
+A sample calculator in python for EDUCATIONAL PURPOSE.
+
+  
+## Installation
+You can download the source file, extract and run the calx.exe
+	-Or
+install using pip
+
+#### Windows
+```sh
+pip install Calx
+or
+python -m pip install Calx
+```
+
+#### Mac or Linux
+```sh
+sudo pip install Calx
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'Calx',  
+  packages = ['Calx'],   
+  version = '1.0',
+  license='MIT',    
+  description = 'Simple Calculator-Using Python and tkinter.',
+  author = 'Mobolaji Abdulsalam',                   
+  author_email = 'agesxpat@gmail.com',
+  url = 'https://github.com/moriire/PyPaystackAPI',  
+  download_url = 'https://github.com/moriire/PyPaystackAPI/archive/v_10.tar.gz',
+  keywords = ['calculator','calx', 'writing a calculator using python', 'python calculato', 'sample calculator'],
+  install_requires=['PySimpleGUI'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3',
+  ],)
